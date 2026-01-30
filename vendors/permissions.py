564283f960b5365cd483d64f7afc849a24b38913
renuka2609 <deshpanderenuka153@gmail.com>
2026-01-30 18:49:56 +0530

    Delete vendors directory

--- vendors/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class CanCreateVendor(BasePermission):
-    def has_permission(self, request, view):
-        if view.action == "create":
-            return request.user.role in ["admin", "requester"]
-        return True
--- vendors/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import VendorViewSet
-
-router = DefaultRouter()
-router.register(r'vendors', VendorViewSet)
-
-urlpatterns = router.urls
