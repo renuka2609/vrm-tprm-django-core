44d6014c78db432457ba86a7f78f005e4193494c
renuka2609 <deshpanderenuka153@gmail.com>
2026-01-30 18:47:38 +0530

    Delete permissions directory

--- permissions/rbac.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminOrRequester(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role in ["Admin", "Requester"]
