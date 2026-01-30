1afe76f5dd257b42e7eaa925feac46b101ffcbe6
renuka2609 <deshpanderenuka153@gmail.com>
2026-01-30 18:49:31 +0530

    Delete templates directory

--- templates/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import TemplateViewSet
-
-router = DefaultRouter()
-router.register("templates", TemplateViewSet, basename="templates")
-
-urlpatterns = router.urls
